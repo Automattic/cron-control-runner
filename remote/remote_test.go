4781bc79629691ce4140dbe1ebc56445223c47c9
Andrea Grassi <andrea.grassi@automattic.com>
2022-03-14 18:20:55 +0100

    removed typos and wrong test

--- remote/remote_test.go
+++ remote/remote_test.go
@@ -7,17 +7,16 @@ import (
 func TestCheckIsJSONObject(t *testing.T) {
 	valuesToTestFalse := []string{
 		"\"Valid JSON String that should be excluded\"",
-		"wrong json string",
-		"\"{\\\"correct\\\":\\\"json object with \\\"}\""}
+		"wrong json string"}
 	valuesToTestTrue := []string{"{\"correct\":\"json object\"}", " { \"correct\" : \"json object with extra spacing\" } "}
 	for _, val := range valuesToTestFalse {
 		if isJSONObject(val) {
-			t.Fatalf("isJSON() returned true for %s while it should've been false", val)
+			t.Fatalf("isJSONObject() returned true for %s while it should've been false", val)
 		}
 	}
 	for _, val := range valuesToTestTrue {
 		if !isJSONObject(val) {
-			t.Fatalf("isJSON() returned false for %s while it should've been false", val)
+			t.Fatalf("isJSONObject() returned false for %s while it should've been true", val)
 		}
 	}
 }

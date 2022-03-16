a2c9ab524fa28bb1e2fd6dec7d364c58aacd975a
Andrea Grassi <andrea.grassi@automattic.com>
2022-03-16 10:26:24 +0100

    Structured JSON tests with table testing to improve readability and usability

--- remote/remote_test.go
+++ remote/remote_test.go
@@ -1,22 +1,32 @@
 package remote
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestCheckIsJSONObject(t *testing.T) {
-	valuesToTestFalse := []string{
-		"\"Valid JSON String that should be excluded\"",
-		"wrong json string"}
-	valuesToTestTrue := []string{"{\"correct\":\"json object\"}", " { \"correct\" : \"json object with extra spacing\" } "}
-	for _, val := range valuesToTestFalse {
-		if isJSONObject(val) {
-			t.Fatalf("isJSONObject() returned true for %s while it should've been false", val)
-		}
+	tests := map[string]struct {
+		input string
+		want  bool
+	}{
+		"normal text with no quotes":                {want: false, input: "normal text"},
+		"array object":                              {want: false, input: "[1,2,3]"},
+		"valid json string that should be excluded": {want: false, input: `"normal text inside quotes"`},
+		"missing quotes json":                       {want: false, input: `{"broken":json"}`},
+		"json inside extra quotes":                  {want: false, input: `"{"broken":"json"}"`},
+		"missing closing parenthesis":               {want: false, input: `{"broken":"json object"`},
+		"wrong numerical key json":                  {want: false, input: ` { 1 : " wrong" } `},
+		"standard json":                             {want: true, input: `{"object":"json"}`},
+		"json with extra spacing":                   {want: true, input: `  { "object space" : "with spacing" } `},
 	}
-	for _, val := range valuesToTestTrue {
-		if !isJSONObject(val) {
-			t.Fatalf("isJSONObject() returned false for %s while it should've been true", val)
-		}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := isJSONObject(tc.input)
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Fatalf("testing '%v' isJSONObject(\"%v\") expected: %v, got: %v", name, tc.input, tc.want, got)
+			}
+		})
 	}
 }

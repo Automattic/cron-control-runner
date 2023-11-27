255ab92a3d21687c9b42eddef732c4bdc21ce347
Rebecca Hum <rebecca.hum@automattic.com>
2023-11-27 13:07:38 -0700

    CANTINA-996: Remove restrictions for `db` and fix `vipsupport`

--- remote/remote.go
+++ remote/remote.go
@@ -60,14 +60,13 @@ var (
 	guidRegex   *regexp.Regexp
 
 	blackListed1stLevel = []string{"admin", "cli", "config", "core", "dist-archive",
-		"eval-file", "eval", "find", "i18n", "scaffold", "server", "package", "profile"}
+		"eval-file", "eval", "find", "i18n", "scaffold", "server", "package", "profile", "vipsupport"}
 
 	blackListed2ndLevel = map[string][]string{
 		"media":    {"regenerate"},
 		"theme":    {"install", "update", "delete"},
 		"plugin":   {"install", "update", "delete"},
 		"language": {"install", "update", "delete"},
-		"vip":      {"support-user"},
 	}
 )
 
@@ -384,15 +383,6 @@ func validateCommand(calledCmd string) (string, error) {
 				strings.ToLower(strings.TrimSpace(cmdParts[1])) == subCommand {
 				return "", fmt.Errorf("WP CLI command '%s %s' is not permitted", command, subCommand)
 			}
-		}
-	}
-
-	if cmdParts[0] == "db" {
-		if cmdParts[1] != "query" {
-			return "", fmt.Errorf("WP CLI command 'db %s' is not permitted", cmdParts[1])
-		}
-		if len(cmdParts) < 3 || cmdParts[2] == "" {
-			return "", errors.New("WP CLI command 'db query' requires a query parameter")
 		}
 	}
 

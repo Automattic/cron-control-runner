74536527259fdbb02de03dcf655da35b0732ddb2
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2025-01-11 04:46:59 +0000

    feat(devcontainer): install `netcat` and `telnet` for debugging

--- .devcontainer/local-features/cron-runner-environment/install.sh
+++ .devcontainer/local-features/cron-runner-environment/install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+apt-get update
+apt-get install -y telnet netcat-openbsd
+rm -rf /var/lib/apt/lists/*
+
 install -D -m 0644 -o root -g root fpm-cron-runner.php /var/wpvip/fpm-cron-runner.php
 install -m 0755 -o root -g root cron-runner-postinstall.sh /var/lib/wordpress/postinstall.d/cron-runner-postinstall
 rm -rf /var/www/*
